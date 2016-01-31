29bd16c601238f59d13fae0cf7c493665d19e6ad
Paul Vacey <pvacey@gmail.com>
2016-01-31 14:57:41 -0600

    the script now fetches 10 pages worth of albums data instead of just the first page.  usage has changes, now accepts a tag name instead of a URL

--- stream.py
+++ stream.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-#https://bandcamp.com/tag/surf-rock
 import re,json,os,sys
 import time
 import random
@@ -7,10 +6,24 @@ import requests
 
 if (len(sys.argv) < 2):
     print 'a simple command line bandcamp radio - https://github.com/pvacey/bandcamp_radio'
-    print '\nusage: \n    '+sys.argv[0]+' <url>'
-    print '    '+sys.argv[0]+' https://bandcamp.com/tag/indie-rock\n'
+    print 'usage: \n    '+sys.argv[0]+' <tag>'
+    print 'example: \n    '+sys.argv[0]+' ambient'
     exit()
-url = sys.argv[1]
+
+def get_album_links(tag):
+    print '***Retrieving Album Data***'
+    albums =[]
+    for i in range(1,11):
+        #for i in range(1,2): # only do 1 page for testing purposes
+        url = 'https://bandcamp.com/tag/'+tag+'?page='+str(i)
+        tagpage = requests.get(url).text
+        tmp = re.findall('href="(.+?)"\s+title',tagpage)
+        if len(tmp) == 0:
+            print '[ERROR] tag "%s" is not valid' % tag
+            exit()
+        for a in tmp:
+            albums.append(a)
+    return albums
 
 def play_random_song(the_url):
     #grab the html of the album page
@@ -40,14 +53,6 @@ def play_random_song(the_url):
     except:
         print '[ERROR] cannot play song, skipping'
 
-# get list of albums
-albums =[]
-tagpage= requests.get(url).text
-tmp = re.findall('class="item ">[\W\w]+?(?=<a)([\W\w]+?)(?=<\/li)',tagpage)
-for a in tmp:
-    a_link = re.findall('href="([^"]+)',a)
-    albums.append(a_link[0])
-    #print a_link[0]
 
 print '''
     ___               __                      ___          ___
@@ -57,6 +62,19 @@ print '''
                                    /_/
 '''
 
+url = sys.argv[1]
+tag = sys.argv[1]
+# get list of albums
+albums = get_album_links(tag)
+'''
+albums =[]
+tagpage= requests.get(url).text
+tmp = re.findall('class="item ">[\W\w]+?(?=<a)([\W\w]+?)(?=<\/li)',tagpage)
+for a in tmp:
+    a_link = re.findall('href="([^"]+)',a)
+    albums.append(a_link[0])
+    #print a_link[0]
+'''
 while True:
     url = albums[random.randrange(len(albums))]
     #print '[Selecting random album] '+ url
